71b1132891d35dc1f13edeefee4eb916d6588083
Osmar Viana <osmarvianatorres@gmail.com>
2023-04-20 14:38:57 -0300

    atualizando informações das vagas

--- assets/js/inscreva-se-mais-info.js
+++ assets/js/inscreva-se-mais-info.js
@@ -1,7 +1,11 @@
 const accordion = document.getElementById("accordion");
+const toggleIcon = document.getElementsByClassName("fa-solid");
 
 accordion.addEventListener("click", (e) => {
   if (e.target.classList.contains("more-information")) {
-    e.target.parentElement.parentElement.classList.toggle("scale");
+    e.target.parentElement.parentElement.parentElement.classList.toggle(
+      "scale"
+    );
+    e.target.children.toggle("fa-minus");
   }
 });
